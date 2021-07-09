a6bce9ee7d068bfa00daec86ef0da7582718efe3
hiyori99 <82105285+hiyori99@users.noreply.github.com>
2021-07-10 00:03:03 +0900

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,1 +1,12 @@
 from time import sleep
+from gpiozero import MotionSensor
+
+def detectIntruders():
+  pir.wait_for_ motion()
+  print('Alert!')
+  sleep(5)
+  
+pir = MoionSensor(18)
+
+while True:
+  DetectIntruders()
