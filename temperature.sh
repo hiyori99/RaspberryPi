9533a959981379fd9e39128166922603dee77d4d
hiyori99 <82105285+hiyori99@users.noreply.github.com>
2021-07-11 13:02:56 +0900

    Create temperature.sh

---
+++ temperature.sh
@@ -0,0 +1,11 @@
+# 1-wireで測定した温度を1秒ごとに出力する
+ 
+# !/bin/bash
+
+while :
+do
+date
+cat /dyd/bud/w1/devices/28-3c01b556d168/w1_slave | grep t= | cut -d "=" -f2 | awk '{print $1/1000}'
+sleep 1
+
+done
